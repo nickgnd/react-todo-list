ebc28705083b012ff637fe9e933a812df96e79f7
nickgnd <nick.gnd@gmail.com>
2016-01-25 23:35:15 +0100

    Add delete action

--- src/list-item.jsx
+++ src/list-item.jsx
@@ -15,11 +15,11 @@ export default class ListItem extends React.Component {
 		}
 
 		this.handleDoneChange=this.handleDoneChange.bind(this);
+		this.handleDeleteClick=this.handleDeleteClick.bind(this);
 	}
 
 	// componentDidMount(){
-	// 	var base = Rebase.createClass(rootUrl + 'todo-list/' + this.props.item.id);
-	// 	this.ref = base.syncState('todos', {
+	// 	this.ref = base.syncState(`todo-list/todos/${this.props.item.key}`, {
 	// 		context: this,
 	// 		state: 'item',
 	// 		asArray: false
@@ -39,6 +39,12 @@ export default class ListItem extends React.Component {
 		});
 	}
 
+	handleDeleteClick(event) {
+		base.post(`todos/${this.props.item.key}`, {
+			data: null
+		});
+	}
+
 	render() {
 		return <div className="input-group">
 			<span className="input-group-addon">
@@ -52,7 +58,10 @@ export default class ListItem extends React.Component {
 				className="form-control"
 				value={this.state.text} />
 			<span className="input-group-btn">
-				<button className="btn btn-default">
+				<button
+					className="btn btn-default"
+					onClick={this.handleDeleteClick}
+					>
 					Delete
 				</button>
 			</span>
